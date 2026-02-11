e6d9e5bc127d8c6d764f6f9327657f62cabcbb43
Lorenzo <llazzaroni@student.ethz.ch>
2026-02-11 15:11:54 +0100

    finalized testing old models

--- exploration/losses_baseline.py
+++ exploration/losses_baseline.py
@@ -114,6 +114,153 @@ def main(args):
             save_path=Path(__file__).resolve().parent / "saccade_evaluation_wrt_poisson.png",
             fig_size=(9, 6),
         )
+        '''
+        eff_dur_raw = bootstrap_mean_difference(
+            -losses["RME_CSS_DUR_RAW"], -losses["RME_CSS_RAW"], N=1000, reduce=True
+        )
+        eff_cs_raw = bootstrap_mean_difference(
+            -losses["RME_CSS_CS_RAW"], -losses["RME_CSS_RAW"], N=1000, reduce=True
+        )
+        eff_ws_raw = bootstrap_mean_difference(
+            -losses["RME_CSS_WS_RAW"], -losses["RME_CSS_RAW"], N=1000, reduce=True
+        )
+        eff_len_raw = bootstrap_mean_difference(
+            -losses["RME_CSS_LEN_RAW"], -losses["RME_CSS_RAW"], N=1000, reduce=True
+        )
+        eff_freq_raw = bootstrap_mean_difference(
+            -losses["RME_CSS_FREQ_RAW"], -losses["RME_CSS_RAW"], N=1000, reduce=True
+        )
+
+        eff_dur_filtered = bootstrap_mean_difference(
+            -losses["RME_CSS_DUR_FILTERED"],
+            -losses["RME_CSS_FILTERED"],
+            N=1000,
+        )
+        eff_cs_filtered = bootstrap_mean_difference(
+            -losses["RME_CSS_CS_FILTERED"],
+            -losses["RME_CSS_FILTERED"],
+            N=1000,
+        )
+        eff_ws_filtered = bootstrap_mean_difference(
+            -losses["RME_CSS_WS_FILTERED"],
+            -losses["RME_CSS_FILTERED"],
+            N=1000,
+        )
+        eff_len_filtered = bootstrap_mean_difference(
+            -losses["RME_CSS_LEN_FILTERED"],
+            -losses["RME_CSS_FILTERED"],
+            N=1000,
+        )
+        eff_freq_filtered = bootstrap_mean_difference(
+            -losses["RME_CSS_FREQ_FILTERED"],
+            -losses["RME_CSS_FILTERED"],
+            N=1000,
+        )
+
+        eff_len_freq_filtered = bootstrap_mean_difference(
+            -losses["RME_CSS_LEN_FREQ_FILTERED"], -losses["RME_CSS_FILTERED"], N=1000, reduce=True
+        )
+        eff_len_freq_raw = bootstrap_mean_difference(
+            -losses["RME_CSS_LEN_FREQ_RAW"], -losses["RME_CSS_RAW"], N=1000, reduce=True
+        )
+        eff_ws_len_freq_filtered = bootstrap_mean_difference(
+            -losses["RME_CSS_WORD_LEN_FREQ_FILTERED"], -losses["RME_CSS_FILTERED"], N=1000, reduce=True
+        )
+        eff_ws_len_freq_raw = bootstrap_mean_difference(
+            -losses["RME_CSS_WORD_LEN_FREQ_RAW"], -losses["RME_CSS_RAW"], N=1000, reduce=True
+        )
+        eff_cs_len_freq_filtered = bootstrap_mean_difference(
+            -losses["RME_CSS_CHAR_LEN_FREQ_FILTERED"], -losses["RME_CSS_FILTERED"], N=1000, reduce=True
+        )
+        eff_cs_len_freq_raw = bootstrap_mean_difference(
+            -losses["RME_CSS_CHAR_LEN_FREQ_RAW"], -losses["RME_CSS_RAW"], N=1000, reduce=True
+        )
+        eff_cs_ws_len_freq_filtered = bootstrap_mean_difference(
+            -losses["RME_CSS_CHAR_WORD_LEN_FREQ_FILTERED"], -losses["RME_CSS_FILTERED"], N=1000, reduce=True
+        )
+        eff_cs_ws_len_freq_raw = bootstrap_mean_difference(
+            -losses["RME_CSS_CHAR_WORD_LEN_FREQ_RAW"], -losses["RME_CSS_RAW"], N=1000, reduce=True
+        )
+        plot_llr_violins(
+            [
+                eff_dur_filtered,
+                eff_dur_raw,
+                eff_cs_filtered,
+                eff_cs_raw,
+                eff_ws_filtered,
+                eff_ws_raw,
+                eff_freq_filtered,
+                eff_freq_raw,
+                eff_len_filtered,
+                eff_len_raw,
+                eff_len_freq_filtered,
+                eff_len_freq_raw,
+                eff_ws_len_freq_filtered,
+                eff_ws_len_freq_raw,
+                eff_cs_len_freq_filtered,
+                eff_cs_len_freq_raw,
+                eff_cs_ws_len_freq_filtered,
+                eff_cs_ws_len_freq_raw,
+            ],
+            labels=[
+                "Duration",
+                "Duration",
+                "Char \n Surp",
+                "Char \n Surp",
+                "Word \n Surp",
+                "Word \n Surp",
+                " Freq",
+                " Freq",
+                "Length",
+                "Length",
+                "Len + Freq",
+                "Len + Freq",
+                "WS Len + Freq",
+                "WS Len + Freq",
+                "CS Len + Freq",
+                "CS Len + Freq",
+                "CS WS Len + Freq",
+                "CS WS Len + Freq",
+            ],
+            title="Bootstrap Estimates of Log-Likelihood Gains on Test Set Across Saccade Models (Extended)",
+            y_label="Delta Log-Likelihood Per Fixation w.r.t. RSE model",
+            save_path=Path(__file__).resolve().parent / "saccade_evaluation_wrt_rse_extended.png",
+            fig_size=(10, 6),
+            step=0.02,
+            dpi=1200,
+        )
+
+        plot_llr_violins(
+            [
+                eff_dur_filtered,
+                eff_cs_filtered,
+                eff_ws_filtered,
+                eff_freq_filtered,
+                eff_len_filtered,
+                eff_len_freq_filtered,
+                eff_ws_len_freq_filtered,
+                eff_cs_len_freq_filtered,
+                eff_cs_ws_len_freq_filtered,
+            ],
+            labels=[
+                "Duration",
+                "Char \n Surp",
+                "Word \n Surp",
+                " Freq",
+                "Length",
+                "Len + Freq",
+                "WS Len + Freq",
+                "CS Len + Freq",
+                "CS WS Len + Freq",
+            ],
+            title="Bootstrap Estimates of Log-Likelihood Gains \n on Test Set Across Saccade Models",
+            y_label="Delta Log-Likelihood Per Fixation w.r.t. RSE Model",
+            save_path=Path(__file__).resolve().parent / "saccade_evaluation_wrt_rse_full_scanpath.png",
+            fig_size=(5, 6),
+            step=0.02,
+            dpi=1000,
+        )
+        '''
 
 
 
@@ -138,4 +285,5 @@ if __name__ == "__main__":
 '''
 python losses.py --models-path "/Users/lorenzolazzaroni/Documents/Programming/Python/Research in DS/spatio-temporal-reading-proj/data" --models RME_CSS_WS_FILTERED RME_CSS_WS_RAW BASE_LF_RAW BASE_SHP_RAW CSS_RAW poisson_raw_baseline RME_CSS_CHAR_LEN_FREQ_RAW RME_CSS_CHAR_WORD_LEN_FREQ_RAW RME_CSS_CS_RAW RME_CSS_DUR_RAW RME_CSS_FREQ_RAW RME_CSS_LEN_FREQ_RAW RME_CSS_LEN_RAW RME_CSS_RAW RME_CSS_WORD_LEN_FREQ_RAW RME_CSS_WS_RAW BASE_LF_FILTERED BASE_SHP_FILTERED CSS_FILTERED poisson_filtered_baseline RME_CSS_CHAR_LEN_FREQ_FILTERED RME_CSS_CHAR_WORD_LEN_FREQ_FILTERED RME_CSS_CS_FILTERED RME_CSS_DUR_FILTERED RME_CSS_FREQ_FILTERED RME_CSS_LEN_FREQ_FILTERED RME_CSS_LEN_FILTERED RME_CSS_FILTERED RME_CSS_WORD_LEN_FREQ_FILTERED RME_CSS_WS_FILTERED TRANSFORMER_FILTERED TRANSFORMER_RAW
 python losses.py --models-path "/Users/lorenzolazzaroni/Documents/Programming/Python/Research in DS/spatio-temporal-reading-proj/data" --models TRANSFORMER_RAW RME_CSS_LEN_FREQ_RAW --plot-distributions
+python losses_baseline.py --models-path "/Users/lorenzolazzaroni/Documents/Programming/Python/Research in DS/spatio-temporal-reading-proj/data" --evaluate-models
 '''
